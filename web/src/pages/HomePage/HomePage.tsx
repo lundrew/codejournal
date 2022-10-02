4c70d16477dc7ae031aa78d1d2a3559f300c8b3d
lundrew <92401835+lundrew@users.noreply.github.com>
2022-10-02 13:20:48 -0700

    landing page ✨

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,16 +1,47 @@
 import { MetaTags } from '@redwoodjs/web'
+import { useAuth } from '@redwoodjs/auth'
+import { Link, routes } from '@redwoodjs/router'
+import './HomePage.css'
 
-import ArticlesCell from 'src/components/ArticlesCell'
-import PostsCell from 'src/components/Post/PostsCell'
+type AdminLayoutProps = {
+  children?: React.ReactNode
+}
 
-const HomePage = () => {
+const HomePage = ({ children }: AdminLayoutProps) => {
+  const { isAuthenticated, currentUser, logOut } = useAuth()
   return (
     <>
       <MetaTags title="Home" description="Home page" />
-
-      <h1>HomePage</h1>
-
-      <ArticlesCell />
+      <h1 className='title'>
+        Code Journal
+      </h1>
+      <div className='container'>
+        <p className='description'>
+          &#60;your personal journal
+        </p>
+        <p className='descriptionTwo'>
+          of code snippets/&#62;
+        </p>
+        <p className='descriptionThree'>
+          learn
+          <br/>
+          store
+          <br/>
+          revisit
+          <br/>
+          retain.
+        </p>
+        <button className='logInButton'><Link to={routes.signup()}>Sign Up</Link></button>
+        <div>
+        {isAuthenticated ? (
+            <div>
+              <button className='signUpButton' onClick={logOut}>Log Out</button>
+            </div>
+          ) : (
+            <button className='signUpButton'><Link to={routes.login()}>Login</Link></button>
+          )}
+        </div>
+      </div>
     </>
   )
 }
